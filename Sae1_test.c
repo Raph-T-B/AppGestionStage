e6fa49d44fa0e30831d91bc3b7c98d4c0c821ff3
FERRANDON <ferrandon4141@gmail.com>
2024-11-06 09:40:28 +0100

    debut test chargement et +

--- Sae1_test.c
+++ Sae1_test.c
@@ -1,5 +1,14 @@
 #include"Sae1.h"
 
+void testChargementEtu(void) {
+    int Tab_Etu={101,124,236,520};
+    int Tab_RSta={-1,2001,-1,5623};
+    float Tab_Note={-1.0,-1.0,-1.0,-1.0};
+    int Tmax=20;
+    chargementEtu(Tab_Etu, Tab_RSta, Tab_Note, Tmax);
+}
+
+
 int main(void){
     return 0;
 }
